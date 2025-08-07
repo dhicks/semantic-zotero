b8360055033bef7b3861ce1f00bec594deb8f7fe
Dan Hicks <hicks.daniel.j@gmail.com>
2025-08-06 21:49:04 -0700

    track similarity scratch to share

---
+++ scratch/mean similarity.R
@@ -0,0 +1,49 @@
+## Check mean similarity across "pages" within each document
+library(tidyverse)
+library(ollamar)
+
+library(here)
+library(assertthat)
+library(glue)
+
+source(here('parameters.R'))
+
+## k x max_dim matrix of document section embeddings
+## for each doc, 1 row = 1 "page" in indexing
+embeds = read_rds(embeds_file)
+
+## Split matrix by doc ----
+# Extract document identifiers
+doc_ids <- sub("\\|\\|.*$", "", rownames(embeds))
+
+# Split matrix rows by doc ID
+result <- tapply(seq_len(nrow(embeds)), doc_ids, 
+                 function(idx) embeds[idx, , drop = FALSE], 
+                 simplify = FALSE)
+
+
+## Within-doc similarities ----
+sims_df = result |> 
+    keep(~ nrow(.x) > 1) |>     ## Only docs w/ multiple pages
+    map(~ tcrossprod(.x)) |> 
+    map(~ .x[upper.tri(.x, diag = FALSE)]) |> 
+    map(~ tibble(sims = list(.x), 
+                 mean = mean(.x), 
+                 sd = sd(.x), 
+                 n = length(.x))) |> 
+    bind_rows(.id = 'doc') |> 
+    select(doc, mean, sd, n, sims)
+
+## Median of mean within-doc similarity is about 0.25; very few have mean > 0.5
+sims_df |> 
+    dplyr::pull(mean) |> 
+    summary()
+
+ggplot(sims_df, aes(mean)) +
+    # stat_density()
+    stat_ecdf()
+
+## Mean of sd is somewhere around 0.1
+## lots of non-finite SDs because a 2x2 will only have 1 off-diagonal element
+ggplot(sims_df, aes(sd)) +
+    stat_density()
